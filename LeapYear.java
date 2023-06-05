d39c2295ffa9c1b814e546fd651989e8782e1afc
graham-hill <135067889+graham-hill@users.noreply.github.com>
2023-06-04 22:58:41 -0500

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,12 @@
+import java.until.Scanner;
+
+public class HW_LeapYear_graham.hill
+{
+   public static void main(String[] args)
+   {
+     int year;
+     Scanner keyboard = new Scanner(System.in);
+     System.out.print("Enter a year: ");
+     year = keyboard.nextInt();
+   }
+}
